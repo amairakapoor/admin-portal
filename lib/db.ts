16f27d603b25ee0e37071e9c279fcd17dfb7fd08
amairakapoor <kapooramaira28@gmail.com>
2026-08-15 20:28:49 +0530

    fix: db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -6,13 +6,15 @@ if (!mongoUri || mongoUri.trim() === "") {
   throw new Error("Please add MONGODB_URI to .env.local");
 }
 
+const mongoUriString = mongoUri.trim();
+
 export async function connectDB() {
   try {
     if (mongoose.connection.readyState === 1) {
       return mongoose.connection;
     }
 
-    await mongoose.connect(mongoUri);
+    await mongoose.connect(mongoUriString);
 
     console.log("MongoDB connected");
 
